c7198827f02d3d8ad9e6270b28d255dde936e101
bat-cha <baptiste.chatrain@gmail.com>
2020-07-31 15:57:41 +0100

    split remte kbd, add head ctrl

--- Code/Server/main_headless.py
+++ Code/Server/main_headless.py
@@ -9,24 +9,6 @@ import getopt
 from Thread import *
 from threading import Thread
 from server import Server
-from Motor import *
-from Buzzer import *
-from evdev import InputDevice, categorize, ecodes
-
-SPACE = 57
-OK = 28
-LEFT = 105
-RIGHT = 106
-UP = 103
-DOWN = 108
-VUP = 115
-VDOWN = 114
-PLAY = 164
-PREV = 165
-NEXT = 163
-CONFIG = 171
-
-remote_keyboard = InputDevice('/dev/input/event0')
 
 class myserver():
     def __init__(self):
@@ -58,29 +40,7 @@ class myserver():
 
 if __name__ == '__main__':
     myshow = myserver()
-    PWM=Motor()
-    buzzer = Buzzer()
     try:
-        for event in remote_keyboard.read_loop():
-            if event.type == ecodes.EV_KEY:
-                if event.value == 0: # release stop
-                    PWM.setMotorModel(0,0,0,0)
-                    buzzer.run('0')
-                    sdcount = 0
-                elif event.value == 1: # press - start
-                    if event.code == UP:
-                        PWM.setMotorModel(1000,1000,1000,1000)      
-                    elif event.code == DOWN:
-                        PWM.setMotorModel(-1000,-1000,-1000,-1000)
-                    elif event.code == LEFT:
-                        PWM.setMotorModel(-1500,-1500,2000,2000)
-                    elif event.code == RIGHT:
-                        PWM.setMotorModel(2000,2000,-1500,-1500)
-                    elif event.code == SPACE:
-                        myshow.on_pushButton()
-                    elif event.code == OK:
-                        buzzer.run('1')
-                    else:
-                        print(categorize(event))
+       pass
     except KeyboardInterrupt:
         myshow.close()
---
+++ Code/Server/main_remote_kbd.py
@@ -0,0 +1,80 @@
+from Motor import *
+from Buzzer import *
+from servo import *
+from evdev import InputDevice, categorize, ecodes
+
+SPACE = 57
+OK = 28
+LEFT = 105
+RIGHT = 106
+UP = 103
+DOWN = 108
+VUP = 115
+VDOWN = 114
+PLAY = 164
+PREV = 165
+NEXT = 163
+CONFIG = 171
+HEAD_UP = 18
+HEAD_DOWN = 32
+HEAD_CENTER = 46
+HEAD_LEFT = 31
+HEAD_RIGHT = 33
+
+remote_keyboard = InputDevice('/dev/input/event0')
+
+if __name__ == '__main__':
+    PWM=Motor()
+    buzzer = Buzzer()
+    servo = Servo()
+    servo0=90
+    servo1=90
+    try:
+        for event in remote_keyboard.read_loop():
+            if event.type == ecodes.EV_KEY:
+                if event.value == 0: # release stop
+                    PWM.setMotorModel(0,0,0,0)
+                    buzzer.run('0')
+                    sdcount = 0
+                elif event.value == 1: # press - start
+                    print(categorize(event))
+                    if event.code == UP:
+                        PWM.setMotorModel(1000,1000,1000,1000)      
+                    elif event.code == DOWN:
+                        PWM.setMotorModel(-1000,-1000,-1000,-1000)
+                    elif event.code == LEFT:
+                        PWM.setMotorModel(-1500,-1500,2000,2000)
+                    elif event.code == RIGHT:
+                        PWM.setMotorModel(2000,2000,-1500,-1500)
+                    elif event.code == SPACE:
+                        myshow.on_pushButton()
+                    elif event.code == OK:
+                        buzzer.run('1')
+                    elif event.code == HEAD_UP:
+                        servo1+=10
+                        if servo1>=180:
+                            servo1=180
+                        servo.setServoPwm('1',servo1)
+                    elif event.code == HEAD_DOWN:
+                        servo1-=10
+                        if servo1<=80:
+                            servo1=80
+                        servo.setServoPwm('1',servo1)
+                    elif event.code == HEAD_LEFT:
+                        servo0-=10
+                        if servo0<=0:
+                            servo0=0
+                        servo.setServoPwm('0',servo0)
+                    elif event.code == HEAD_RIGHT:
+                        servo0+=10
+                        if servo0>=180:
+                            servo0=180
+                        servo.setServoPwm('0',servo0)
+                    elif event.code == HEAD_CENTER:
+                        servo.setServoPwm('0',90)
+                        servo.setServoPwm('1',90)
+
+                    else:
+                        print(categorize(event))
+    except KeyboardInterrupt:
+        print('interrupt')
